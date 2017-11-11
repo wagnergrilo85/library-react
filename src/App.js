6c34d3ff1795b05d67b554981dd13f9372207392
Wagner Grilo de Barros Junior <wagner.grilo85@gmail.com>
2017-11-11 18:27:52 -0200

    mudando o sistema de rotas do 3 para react-router 4 para aplicacoes com react native

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router';
+import {Link} from 'react-router-dom';
+
 
 class App extends Component {
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './css/style.css';
-import {Router, Route, IndexRoute} from 'react-router';
-import { browserHistory } from 'react-router';
+// import {Router, Route, IndexRoute} from 'react-router';
+// import { browserHistory } from 'react-router';
+import {BrowserRouter as Router, Route,Switch,Link} from 'react-router-dom';
+
 import registerServiceWorker from './registerServiceWorker';
 
 import Book from './components/pages/Book';
@@ -12,14 +14,26 @@ import Dashoboard from './components/pages/Dashoboard';
 import Category from './components/pages/Category';
 
 ReactDOM.render(
+    // (
+    //     <Router history={browserHistory}>
+    //         <Route path="/" component={App}>
+    //             <IndexRoute component={Dashoboard} />
+    //             <Route path="/book" component={Book} />
+    //             <Route path="/author" component={Author} />
+    //             <Route path="/category" component={Category} />
+    //         </Route>
+    //     </Router>
+    // ),
     (
-        <Router history={browserHistory}>
-            <Route path="/" component={App}>
-                <IndexRoute component={Dashoboard} />
-                <Route path="/book" component={Book} />
-                <Route path="/author" component={Author} />
-                <Route path="/category" component={Category} />
-            </Route>
+        <Router>
+            <App>
+                <Switch>
+                    <Route exact path="/" component={Dashoboard}/>
+                    <Route path="/book" component={Book}/>
+                    <Route path="/author" component={Author}/>
+                    <Route path="/category" component={Category}/>
+                </Switch>
+            </App>
         </Router>
     ),
     document.getElementById('root')
